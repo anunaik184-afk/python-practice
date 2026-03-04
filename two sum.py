8758ed51c2c6c534ae6f45f5e231d5b5057126f9
anunaik184-afk <anunaik184@gmail.com>
2026-03-04 21:19:25 +0530

    Create two sum.py

---
+++ two sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            if complement in seen:
+                return [seen[complement], i]
+            
+            seen[num] = i
+s=Solution()
+print(s.twoSum[[2,7,11,15],9])
